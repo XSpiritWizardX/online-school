0340dbd58b670459723e0d51c6b33358d1e8593f
daniel watson <ozzloy@each.do>
2025-07-14 12:09:26 -0700

    read backend port from backend .env files

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
+
+const backendPath = path.resolve(__dirname, "../back");
+
+dotenv.config({ path: path.join(backendPath, ".env.local") });
+dotenv.config({ path: path.join(backendPath, ".env.development") });
+dotenv.config({ path: path.join(backendPath, ".env.production") });
+
+const getBackendPort = (mode) => {
+  return "FLASK_RUN_PORT" in process.env
+    ? Number(process.env.FLASK_RUN_PORT)
+    : mode === "production"
+      ? 8000
+      : 5000;
+};
 
 // https://vitejs.dev/config/
-export default defineConfig((mode) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     eslintPlugin({
@@ -12,9 +28,11 @@ export default defineConfig((mode) => ({
     }),
   ],
   server: {
+    // opens browser after starting server
     open: true,
+
     proxy: {
-      "/api": "http://127.0.0.1:8000",
+      "/api": `http://127.0.0.1:${getBackendPort(mode)}`,
     },
   },
 }));
