cee7898f6e1015062c8e03462484c304bc9de51c
daniel watson <ozzloy@each.do>
2025-07-15 18:46:27 -0700

    determine back port during front startup

--- front/README.md
+++ front/README.md
@@ -3,7 +3,45 @@
 ```bash
 cd front/ # cd into the online-school/front directory
 npm install # install/update js packages
-npm run dev # run node with those packages
 ```
 
-browse to [`localhost:5173`]
+## run development
+
+```bash
+npm run dev
+```
+
+### use a browser to view front end
+
+browse to [`localhost:5173`] (or whatever port is configured)
+
+## configuration
+
+vite attempts to read configuration from the following files in the
+following order.  settings in later files override settings in earlier
+files.
+
+| file                                    | in git? |
+| :-------------------------------------- | :-----: |
+| `.env`                                  |   ✅    |
+| `.env.local`                            |   ❌    |
+| `.env.[mode]` (e.g. `.env.development`) |   ✅    |
+| `.env.[mode].local`                     |   ❌    |
+
+`mode` is determined by how the app is started.
+
+| how it started   | mode          |
+| :--------------- | ------------: |
+| `npm run dev`    | "development" |
+| `npm run build`  | "production"  |
+
+## build for production
+
+read port (and other configuration) from `.env.production`
+```bash
+npm run build
+```
+
+### deploy
+TODO
+
--- front/vite.config.js
+++ front/vite.config.js
@@ -4,35 +4,56 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
 
-const backendPath = path.resolve(__dirname, "../back");
+const getBackendEnvironment = () => {
+  const backendPath = path.resolve(__dirname, "../back");
+  const flaskenvPath = path.join(backendPath, ".flaskenv");
+  const flaskEnv = dotenv.config({ path: flaskenvPath });
 
-dotenv.config({ path: path.join(backendPath, ".env.local") });
-dotenv.config({ path: path.join(backendPath, ".env.development") });
-dotenv.config({ path: path.join(backendPath, ".env.production") });
+  const envPath = path.join(backendPath, ".env");
+  const env = dotenv.config({ path: envPath });
 
-const getBackendPort = (mode) => {
-  return "FLASK_RUN_PORT" in process.env
-    ? Number(process.env.FLASK_RUN_PORT)
-    : mode === "production"
-      ? 8000
-      : 5000;
+  const backendEnvironment = { ...(flaskEnv || {}), ...(env || {}) };
+
+  if (!("FLASK_RUN_PORT" in backendEnvironment)) {
+    console.error("unable to determine backend port");
+    process.exit(1);
+  }
+
+  return {
+    ...backendEnvironment,
+    FLASK_RUN_PORT: parseInt(backendEnvironment[FLASK_RUN_PORT], 10),
+  };
+};
+
+const getBackendPort = () => {
+  return getBackendEnvironment["FLASK_RUN_PORT"];
 };
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    eslintPlugin({
-      lintOnStart: true,
-      failOnError: mode === "production",
-    }),
-  ],
-  server: {
-    // opens browser after starting server
-    open: true,
+export default defineConfig(({ mode }) => {
+  const environment = loadEnv(mode, process.cwd(), "");
+  if (!("PORT" in environment)) {
+    console.error(`specify PORT in .env.${mode}`);
+    process.exit(1);
+  }
+  const port = parseInt(environment.PORT, 10);
+  return {
+    plugins: [
+      react(),
+      eslintPlugin({
+        lintOnStart: true,
+        failOnError: mode === "production",
+      }),
+    ],
+    server: {
+      // opens browser after starting server
+      open: true,
 
-    proxy: {
-      "/api": `http://127.0.0.1:${getBackendPort(mode)}`,
+      port,
+
+      proxy: {
+        "/api": `http://127.0.0.1:${getBackendPort()}`,
+      },
     },
-  },
-}));
+  };
+});
