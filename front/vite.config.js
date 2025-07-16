6c333396a70fa5b8beb8847b1ed6316396d35e97
daniel watson <ozzloy@each.do>
2025-07-16 13:24:19 -0700

    gracefully handle case where backend config files do not exist

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,40 +3,53 @@ import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
+import fs from "fs";
 
 const getBackendEnvironment = () => {
   const backendPath = path.resolve(__dirname, "../back");
   const flaskenvPath = path.join(backendPath, ".flaskenv");
-  const flaskEnv = dotenv.config({ path: flaskenvPath });
-
   const envPath = path.join(backendPath, ".env");
-  const env = dotenv.config({ path: envPath });
 
-  const backendEnvironment = { ...(flaskEnv || {}), ...(env || {}) };
+  const flaskenv = fs.existsSync(flaskenvPath)
+    ? dotenv.config({ path: flaskenvPath }).parsed || {}
+    : {};
+
+  const env = fs.existsSync(envPath)
+    ? dotenv.config({ path: envPath }).parsed || {}
+    : {};
+
+  const backendEnvironment = { ...flaskenv, ...env };
 
   if (!("FLASK_RUN_PORT" in backendEnvironment)) {
     console.error("unable to determine backend port");
     process.exit(1);
   }
 
-  return {
+  const FLASK_RUN_PORT = backendEnvironment["FLASK_RUN_PORT"];
+  const flaskRunPort = parseInt(FLASK_RUN_PORT);
+
+  const result = {
     ...backendEnvironment,
-    FLASK_RUN_PORT: parseInt(backendEnvironment[FLASK_RUN_PORT], 10),
+    FLASK_RUN_PORT: flaskRunPort,
   };
+  return result;
 };
 
 const getBackendPort = () => {
-  return getBackendEnvironment["FLASK_RUN_PORT"];
+  return getBackendEnvironment()["FLASK_RUN_PORT"];
 };
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const environment = loadEnv(mode, process.cwd(), "");
   if (!("PORT" in environment)) {
-    console.error(`specify PORT in .env.${mode}`);
+    console.error("unable to determine PORT");
+    console.error("should be set in one of the following files:");
+    console.error(`.env, .env.${mode}, .env.${mode}.local`);
     process.exit(1);
   }
   const port = parseInt(environment.PORT, 10);
+  const api = `http://localhost:${getBackendPort()}`;
   return {
     plugins: [
       react(),
@@ -52,7 +65,7 @@ export default defineConfig(({ mode }) => {
       port,
 
       proxy: {
-        "/api": `http://127.0.0.1:${getBackendPort()}`,
+        "/api": api,
       },
     },
   };
