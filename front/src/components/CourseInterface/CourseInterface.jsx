89498f6fdbc7034ca417f763643436fcc13fec75
MumbisDev <33399197+MumbisDev@users.noreply.github.com>
2025-07-24 21:32:26 -0400

    initial setup

---
+++ front/src/components/CourseInterface/CourseInterface.jsx
@@ -0,0 +1,15 @@
+import { useState, useRef, useEffect } from "react";
+import "./CourseInterface.css"; 
+
+export default function CourseInterface() {
+  return (
+    <div className="course-interface">
+      <h1>Course Interface</h1>
+      <p>This is the course interface component.</p>
+      <p>This is the course interface component.</p>
+      <p>This is the course interface component.</p>
+      <p>This is the course interface component.</p>
+    </div>
+  );
+}
+  
