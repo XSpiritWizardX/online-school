2df5047654959e6da64b9e60dcde296b42dfb6ec
daniel watson <ozzloy@each.do>
2025-07-30 11:02:50 -0700

    add user_emails for use in other seeds

--- back/app/seeds/users.py
+++ back/app/seeds/users.py
@@ -1,10 +1,15 @@
 from app.db import db
 from app.models.user import User
 
+# declare content of each user to seed
 user_seeds = [
     {"email": "admin@example.io", "password": "password"},
     {"email": "normie@example.com", "password": "password"},
 ]
+
+# keep a list of unique identifiers for seeded users for use in other
+# seeds, so they can dynamically determine things like "user_id"
+user_emails = map(lambda user_seed: user_seed["email"], user_seeds)
 
 
 def seed_users():
