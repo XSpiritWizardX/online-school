6893a752afc49b28170e8b1595a8a617c4cca2e1
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-08-07 21:24:21 -0400

    ADD: folder and files, added basic jsx boiler

---
+++ front/src/components/Course/CourseShow/CourseShow.jsx
@@ -0,0 +1,21 @@
+import "./CourseShow.css";
+
+function CourseShow() {
+
+
+// use state and store to get course by id
+
+  return (
+    <div className="c-courses-page-container">
+
+
+        <div className="c-courses-header">
+          <h1>CourseShow</h1>
+        </div>
+        {/* Add the single course by course id here */}
+
+    </div>
+  );
+}
+
+export default CourseShow;
