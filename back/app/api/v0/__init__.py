b99405a4fbadd68af3d1ccae93cf2dad5073b67e
daniel watson <ozzloy@each.do>
2025-08-18 13:52:52 -0700

    add get current users courses

--- back/app/api/v0/__init__.py
+++ back/app/api/v0/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 
 from app.api.v0.auth import bp as auth_bp
+from app.api.v0.courses import bp as course_bp
 
 
 bp = Blueprint("v0", __name__)
 
 bp.register_blueprint(auth_bp, url_prefix="/auth")
+bp.register_blueprint(course_bp, url_prefix="/courses")
