fbcca91932f259fec23139dca604bd1b2ac9c44d
daniel watson <ozzloy@each.do>
2025-09-05 11:53:12 -0700

    add fields to course in backend

--- back/app/seeds/courses.py
+++ back/app/seeds/courses.py
@@ -7,10 +7,22 @@ course_seeds = [
     {
         "title": "Introduction to Python",
         "description": "Learn the basics of Python programming.",
+        "language": "english",
+        "level": "advanced",
+        "price": 0.02,
+        "is_published": True,
+        "preview_video_url": None,
+        "image_url": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     },
     {
         "title": "Advanced JavaScript",
         "description": "Dive into JavaScript and its frameworks.",
+        "language": "english",
+        "level": "beginner",
+        "price": 9_999_999,
+        "is_published": False,
+        "preview_video_url": None,
+        "image_url": None,
     },
 ]
 
@@ -21,11 +33,8 @@ def seed_courses():
     if not users:
         raise ValueError("No users found to assign as course owners.")
     for index, course_seed in enumerate(course_seeds):
-        course = Course(
-            owner_id=users[index % len(users)].id,
-            title=course_seed["title"],
-            description=course_seed["description"],
-        )
+        course = Course(**course_seed)
+        course.owner_id = users[index % len(users)].id
         db.session.add(course)
     db.session.commit()
 
