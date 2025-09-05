4ec7860f38b3d55b76c46bc07d9d2f5b365e9002
daniel watson <ozzloy@each.do>
2025-09-05 15:27:35 -0700

    implement update backend for course

--- back/app/api/v0/courses.py
+++ back/app/api/v0/courses.py
@@ -13,12 +13,8 @@ bp = Blueprint("courses", __name__)
 def get_courses():
     """get all courses for the current user"""
     courses = Course.query.all()
-    return (
-        jsonify(
-            {"courses": [course.to_dict() for course in courses]}
-        ),
-        200,
-    )
+    body = {"courses": [course.to_dict() for course in courses]}
+    return jsonify(body), 200
 
 
 @bp.route("/", methods=["POST"])
@@ -30,6 +26,8 @@ def create_course(current_user):
     if not data:
         return jsonify({"message": "need json body"}), 400
 
+    request_fields = set(data.keys())
+
     required_fields = {
         "title",
         "language",
@@ -37,36 +35,25 @@ def create_course(current_user):
         "price",
         "is_published",
     }
-
-    allowed_fields = required_fields | {
+    optional_fields = {
         "description",
         "preview_video_url",
         "image_url",
     }
+    allowed_fields = required_fields | optional_fields
 
-    rejected_fields = sorted(
-        [key for key in data if key not in allowed_fields]
-    )
-
-    missing_fields = sorted(
-        [key for key in required_fields if key not in data]
-    )
+    rejected_fields = request_fields - allowed_fields
+    missing_fields = required_fields - request_fields
 
     if rejected_fields or missing_fields:
-        request_fields = sorted(list(data.keys()))
-        return (
-            jsonify(
-                {
-                    "message": "rejected and/or missing fields",
-                    "missing_fields": missing_fields,
-                    "rejected_fields": rejected_fields,
-                    "required_fields": sorted(list(required_fields)),
-                    "allowed_fields": sorted(list(allowed_fields)),
-                    "request_fields": request_fields,
-                }
-            ),
-            400,
-        )
+        body = {
+            "message": "rejected and/or missing fields",
+            "missing_fields": sorted(list(missing_fields)),
+            "rejected_fields": sorted(list(rejected_fields)),
+            "required_fields": sorted(list(required_fields)),
+            "optional_fields": sorted(list(optional_fields)),
+        }
+        return jsonify(body), 400
 
     data["owner_id"] = current_user.id
 
@@ -81,12 +68,9 @@ def create_course(current_user):
             "UNIQUE constraint failed: courses.owner_id, courses.title"
             in str(e)
         ):
-            return (
-                jsonify(
-                    {"message": "duplicate course title for user"}
-                ),
-                409,
-            )
+            body = {"message": "duplicate course title for user"}
+            return jsonify(body), 409
+
         else:
             print(f"IntegrityError in {__file__}")
             traceback.print_exc()
@@ -96,15 +80,11 @@ def create_course(current_user):
         traceback.print_exc(e)
         return jsonify({"message": "error creating course"}), 500
 
-    return (
-        jsonify(
-            {
-                "message": "course created successfully",
-                "course": course.to_dict(),
-            }
-        ),
-        201,
-    )
+    body = {
+        "message": "course created successfully",
+        "course": course.to_dict(),
+    }
+    return jsonify(body), 201
 
 
 @bp.route("/<int:course_id>/", methods=["GET"])
@@ -117,6 +97,71 @@ def get_course(course_id):
     if not course:
         return jsonify({"message": "course not found"}), 404
     return jsonify({"course": course.to_dict()}), 200
+
+
+@bp.route("/<int:course_id>/", methods=["PATCH"])
+@jwt_required
+def update_course(current_user, course_id):
+    """modify an existing course"""
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"message": "need json body"}), 400
+
+    request_fields = set(data.keys())
+
+    required_fields = set([])
+    optional_fields = {
+        "owner_id",
+        "title",
+        "description",
+        "language",
+        "level",
+        "price",
+        "is_published",
+        "preview_video_url",
+        "image_url",
+    }
+    allowed_fields = required_fields | optional_fields
+
+    rejected_fields = request_fields - allowed_fields
+    missing_fields = required_fields - request_fields
+
+    if rejected_fields or missing_fields:
+        body = {
+            "message": "rejected and/or missing fields",
+            "missing_fields": sorted(list(missing_fields)),
+            "rejected_fields": sorted(list(rejected_fields)),
+            "required_fields": sorted(list(required_fields)),
+            "optional_fields": sorted(list(optional_fields)),
+        }
+        return jsonify(body), 400
+
+    course = Course.query.get(course_id)
+    if not course:
+        return jsonify({"message": "course not found"}), 404
+
+    if course.owner_id != current_user.id:
+        body = {"message": "not allowed to change this course"}
+        return jsonify(body), 401
+
+    # do the actual update
+    for field in data:
+        setattr(course, field, data[field])
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(f"error updating course in {__file__}")
+        print(e)
+        traceback.print_exc()
+        return jsonify({"message": "error updating course"}), 500
+    body = {
+        "message": "course updated successfully",
+        "course": course.to_dict(),
+    }
+    return jsonify(body), 200
 
 
 @bp.route("/<int:course_id>/", methods=["DELETE"])
